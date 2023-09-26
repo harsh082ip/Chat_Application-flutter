bb1dd8b9f93c71b8621d81645ee0be4f8c707024
ank809 <ankita51523@gmail.com>
2023-09-27 00:57:20 +0530

    users model modified

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -1,1 +1,27 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class MyUser{
+  String name;
+  String email;
+  String uid;
+
+  MyUser({required this.name, required this.email, required this.uid});
+
+  // app --> firebase
+
+  Map<String, dynamic> toJson(){
+    return {
+      'name':name,
+      'email':email,
+      'uid':uid
+    };
+  }
+
+  // firebase --> app
+
+ static MyUser fromSnap(DocumentSnapshot snap){
+    var snapshot= snap.data() as Map<String, dynamic>;
+    return MyUser(name: snapshot['name'], email: snapshot['email'], uid: snapshot['uid']);
+  }
+}
